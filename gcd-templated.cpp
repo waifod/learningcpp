426d310230809b2bf2f5f69c70ba277e195d6e11
Matteo Durante <dumatte@amazon.com>
2024-07-13 13:17:43 +0200

    GCD templated

---
+++ gcd-templated.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+
+template<int m, int n>
+struct GCD {
+  static constexpr int value{GCD<n, m%n>::value};
+};
+
+template<int n>
+struct GCD<0, n> {
+  static constexpr int value{n};
+};
+
+template<int m>
+struct GCD<m, 0> {
+  static constexpr int value{m};
+};
+
+int main() {
+  static_assert(GCD<2, 4>::value == 2);
+  static_assert(GCD<1, 4>::value == 1);
+  static_assert(GCD<0, 4>::value == 4);
+  static_assert(GCD<3, 0>::value == 3);
+  static_assert(GCD<6, 4>::value == 2);
+
+  return EXIT_SUCCESS;
+}
